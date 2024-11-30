30a813dcb746c16537b4a33d702c05abf4011e39
JonathanFPJV <jonathan.juli@tecsup.edu.pe>
2024-11-30 05:42:40 -0500

    Implementación de filtros

--- yugioh_api/cards/views.py
+++ yugioh_api/cards/views.py
@@ -24,11 +24,25 @@ class CustomPagination(PageNumberPagination):
 class BuscarCartaView(APIView):
     def get(self, request):
         name = request.query_params.get('name', None)
+        card_type = request.query_params.get('card_type', None)
+        rarity = request.query_params.get('rarity', None)
+        level = request.query_params.get('level', None)
+
+        # Filtrar las cartas según los parámetros recibidos
+        cartas = Card.objects.all()
+
         if name:
-            # Ordena las cartas por un campo específico
-            cartas = Card.objects.filter(name__icontains=name).order_by('id')
-            paginator = CustomPagination()
-            paginated_cartas = paginator.paginate_queryset(cartas, request)
-            serializer = CardSerializer(paginated_cartas, many=True)
-            return paginator.get_paginated_response(serializer.data)
-        return Response({"error": "Debe proporcionar un nombre para buscar"}, status=status.HTTP_400_BAD_REQUEST)
+            cartas = cartas.filter(name__icontains=name)
+        if card_type:
+            cartas = cartas.filter(card_type__icontains=card_type)
+        if rarity:
+            cartas = cartas.filter(rarity__icontains=rarity)
+        if level:
+            cartas = cartas.filter(level=level)  # Suponiendo que el nivel es un número entero
+
+        # Ordenar las cartas y aplicar la paginación
+        cartas = cartas.order_by('id')
+        paginator = CustomPagination()
+        paginated_cartas = paginator.paginate_queryset(cartas, request)
+        serializer = CardSerializer(paginated_cartas, many=True)
+        return paginator.get_paginated_response(serializer.data)
